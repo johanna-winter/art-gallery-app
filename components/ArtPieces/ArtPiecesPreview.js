051599753292f1fcbd9afe2653190a6deb9dfb44
Elisa Linnemannstoens <elisalin2001@gmail.com>
2025-11-20 10:30:23 +0100

    refactoring

--- components/ArtPieces/ArtPiecesPreview.js
+++ components/ArtPieces/ArtPiecesPreview.js
@@ -1,6 +1,12 @@
 import Image from "next/image";
 import Link from "next/link";
-import { ContainerFavoriteButtonStyled, PreviewImageContainer, PreviewInformationStyled, PreviewPageWrapper, PreviewStyledImage } from "../Preview/PreviewStyled";
+import {
+  ContainerFavoriteButtonStyled,
+  PreviewImageContainer,
+  PreviewInformationStyled,
+  PreviewPageWrapper,
+  PreviewStyledImage,
+} from "./Layout/PreviewStyled";
 import { VertikalerStrich } from "../Comments/CommentsInputStyled";
 import FavoriteButton from "../FavoriteButton/FavoriteButton";
 import { FavoriteButtonContainer } from "../FavoriteButton/FavoriteButtonStyled";
@@ -18,28 +24,30 @@ export default function ArtPiecesPreview({
   return (
     <>
       <PreviewPageWrapper>
-       <PreviewImageContainer>
-        <Link href={`/art-pieces/${slug}`}>
-        <PreviewStyledImage 
-        src={image} 
-        alt={title} 
-        width={300} 
-        height={300} ></PreviewStyledImage>
-      </Link>
-      <ContainerFavoriteButtonStyled>
-      <FavoriteButton
-        slug={slug}
-        onToggle={() => onToggle(slug)}
-        isFavorite={isFavorite}/>
-        </ContainerFavoriteButtonStyled> 
+        <PreviewImageContainer>
+          <Link href={`/art-pieces/${slug}`}>
+            <PreviewStyledImage
+              src={image}
+              alt={title}
+              width={300}
+              height={300}
+            ></PreviewStyledImage>
+          </Link>
+          <ContainerFavoriteButtonStyled>
+            <FavoriteButton
+              slug={slug}
+              onToggle={() => onToggle(slug)}
+              isFavorite={isFavorite}
+            />
+          </ContainerFavoriteButtonStyled>
         </PreviewImageContainer>
-       </PreviewPageWrapper>
+      </PreviewPageWrapper>
       <PreviewInformationStyled>
-      <h4>{title}</h4>
-      <p>{artist}</p>
+        <h4>{title}</h4>
+        <p>{artist}</p>
       </PreviewInformationStyled>
-      
-          <VertikalerStrich/>
+
+      <VertikalerStrich />
     </>
   );
 }
