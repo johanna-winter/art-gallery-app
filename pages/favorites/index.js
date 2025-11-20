0d22437afd3db373e52f8453bc86248674f5fa13
Johanna Winter <johanna.winter@gmx.de>
2025-11-20 10:42:56 +0100

    add message when favorites are empty

--- pages/favorites/index.js
+++ pages/favorites/index.js
@@ -1,10 +1,19 @@
 import ArtPiecesPreview from "@/components/ArtPieces/ArtPiecesPreview";
 import { PreviewPageWrapper } from "@/components/ArtPieces/Layout/PreviewStyled";
+import { SpotlightHeader } from "@/components/Spotlight/SpotlightStyled";
 
 export default function FavoritesList({ onToggle, artPieces, favoritesData }) {
   const favoriteArtPiecesList = artPieces.filter((artpiece) =>
     favoritesData.includes(artpiece.slug)
   );
+
+  if (favoriteArtPiecesList.length === 0) {
+    return (
+      <SpotlightHeader>
+        <strong>Please add some favorite art from the gallery.</strong>
+      </SpotlightHeader>
+    );
+  }
 
   return (
     <>
